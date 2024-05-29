87c29263e9c84f96108dbefe027b09803f18da28
Arturo <arturo.reyes.c01@mail.pucv.cl>
2024-05-29 19:37:35 -0400

    comentarios al logout

--- backend/src/controllers/logout.controller.ts
+++ backend/src/controllers/logout.controller.ts
@@ -5,7 +5,11 @@ export const logout = (req: Request, res: Response) => {
     if (err) {
       return res.status(500).json({ message: 'Error al cerrar sesión' });
     }
+
+    // Eliminar la cookie de sesión
     res.clearCookie('connect.sid');
+
+    // Enviar respuesta de éxito
     res.json({ message: 'Cierre de sesión exitoso' });
   });
 };
