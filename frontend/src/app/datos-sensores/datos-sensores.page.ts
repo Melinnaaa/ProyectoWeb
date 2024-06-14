0aad864c37494c4e7cc12aed6fbf4797745a3db8
Arturo <arturo.reyes.c01@mail.pucv.cl>
2024-06-14 02:08:27 -0400

    Conexión con robot

--- frontend/src/app/datos-sensores/datos-sensores.page.ts
+++ frontend/src/app/datos-sensores/datos-sensores.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { SensorService } from '../services/sensor.service';
 
 @Component({
   selector: 'app-datos-sensores',
@@ -6,10 +7,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./datos-sensores.page.scss'],
 })
 export class DatosSensoresPage implements OnInit {
+  sensorData: any;
 
-  constructor() { }
+  constructor(private sensorService: SensorService) { }
 
   ngOnInit() {
+    this.getSensorData();
   }
 
+  getSensorData() {
+    this.sensorService.getSensorData().subscribe(data => {
+      this.sensorData = data;
+      console.log(this.sensorData); // Para verificar que los datos están siendo recibidos
+    });
+  }
 }
