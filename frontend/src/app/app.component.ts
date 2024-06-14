e926d279ae7636a2e726ca4c0ea2d4e8e92039ef
Arturo <arturo.reyes.c01@mail.pucv.cl>
2024-06-14 02:26:16 -0400

    Arreglar error de footer y header

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -8,23 +8,42 @@ import { Location } from '@angular/common';
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  mostrarFooter = true;
-  mostrarBoton = true;
+  mostrarFooter: boolean;
+  mostrarBoton: boolean;
 
   constructor(private router: Router, private location: Location) {
-    this.router.events.subscribe((event) => 
-    {
-      // Se muestra la tolbar en pags especificas
+    this.mostrarFooter = this.getCookie('mostrarFooter') !== 'false';
+    this.mostrarBoton = this.getCookie('mostrarBoton') !== 'false';
+
+    this.router.events.subscribe((event) => {
       if (event instanceof NavigationEnd) {
-        this.mostrarFooter = !['/login', '/register', '/home'].includes(event.url); // Esconde el footer en 'login' y 'registro'
-      }
-      if (event instanceof NavigationEnd) {
-        this.mostrarBoton = ![ '/principal', '/home'].includes(event.url); // Esconde el boton
+        const currentUrl = event.urlAfterRedirects || event.url;
+        console.log('Navigated to:', currentUrl);
+
+        this.mostrarFooter = !['/login', '/register', '/home'].includes(currentUrl);
+        this.mostrarBoton = !['/principal', '/home'].includes(currentUrl);
+
+        document.cookie = `mostrarFooter=${this.mostrarFooter}; path=/`;
+        document.cookie = `mostrarBoton=${this.mostrarBoton}; path=/`;
+
+        console.log('Updated mostrarFooter:', this.mostrarFooter);
+        console.log('Updated mostrarBoton:', this.mostrarBoton);
       }
     });
   }
 
   goBack() {
     this.location.back(); // Esto llevará al usuario a la página anterior
+  }
+
+  private getCookie(name: string): string {
+    const nameEQ = name + "=";
+    const ca = document.cookie.split(';');
+    for (let i = 0; i < ca.length; i++) {
+      let c = ca[i];
+      while (c.charAt(0) == ' ') c = c.substring(1, c.length);
+      if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
+    }
+    return '';
   }
 }
