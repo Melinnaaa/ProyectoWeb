438300d02a90bb332cd89a6e936fbf92df802d19
Arturo <arturo.reyes.c01@mail.pucv.cl>
2024-06-16 14:19:25 -0400

    subir app a la web

--- backend/src/config/config.ts
+++ backend/src/config/config.ts
@@ -1,14 +1,15 @@
 import dotenv from 'dotenv';
 dotenv.config();
+
 const config = {
-    port: process.env.PORT || '3001',
-    db: {
-      host: 'localhost',
-      user: 'root',
-      password: '',
-      database: 'mascotapp'
-    }
-  };
+  port: process.env.PORT || '3001',
+  db: {
+    host: process.env.DB_HOST || 'localhost',
+    user: process.env.DB_USER || 'root',
+    password: process.env.DB_PASSWORD || '',
+    database: process.env.DB_NAME || 'mascotapp'
+  }
+};
   
   export default config;
   
